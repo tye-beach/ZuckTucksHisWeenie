2af5294cf6db99c15de3dd0fc0e2328fc49e292c
tye-beach <55841503+tye-beach@users.noreply.github.com>
2019-12-08 12:45:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from discord.ext import commands
 import utils.data as data
 import utils.roles
 
-print ('Tye BeacH')
 
 description = '''Aventus Bot'''
 
@@ -22,7 +21,7 @@ async def on_ready():
 
     print(bot.user.name)
     print(bot.user.id)
-    data.server = bot.get_server('391626662351077386')
+    data.server = bot.get_server('652913837003243563')
     members = data.server.members
     online_members = list(filter(lambda x: not x.status == discord.Status.offline, members))
     await bot.change_presence(
