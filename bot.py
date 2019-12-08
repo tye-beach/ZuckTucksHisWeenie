c86a6618cdc850d7c7adc88f681b495306d840a6
tye-beach <55841503+tye-beach@users.noreply.github.com>
2019-12-08 13:02:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def on_ready():
 
     print(bot.user.name)
     print(bot.user.id)
-    data.server = bot.get_server('652913837003243563')
+    data.server = bot.get_server('391626662351077386')
     members = data.server.members
     online_members = list(filter(lambda x: not x.status == discord.Status.offline, members))
     await bot.change_presence(
